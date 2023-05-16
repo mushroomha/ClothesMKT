6b84794d2a12da06e46b40af2a70db2a33c6e72a
mushroomha <mushroomha@qq.com>
2023-05-16 20:11:39 +0800

    welcome class update

---
+++ untitled/src/com/vince/MainClass.java
@@ -0,0 +1,13 @@
+package com.vince;
+/*
+* 程序的入口类
+* */
+
+
+import com.vince.ui.WelcomeClass;
+
+public class MainClass {
+    public static void main(String[] args) {
+        new WelcomeClass().start();
+    }
+}
