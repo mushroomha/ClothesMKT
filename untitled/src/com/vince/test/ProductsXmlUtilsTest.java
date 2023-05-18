6acd2f892a9b93f4697e413e825185fdf90b85f4
mushroomha <mushroomha@qq.com>
2023-05-18 21:33:54 +0800

    xml analysis and test

---
+++ untitled/src/com/vince/test/ProductsXmlUtilsTest.java
@@ -0,0 +1,18 @@
+package com.vince.test;
+
+import com.vince.bean.Clothes;
+import com.vince.utils.ProductsXmlUtils;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ProductsXmlUtilsTest {
+
+    @Test
+    public void test(){
+        List<Clothes> clothes = ProductsXmlUtils.parserProductFromXml();
+        System.out.println(Arrays.toString(clothes.toArray()));
+    }
+}
